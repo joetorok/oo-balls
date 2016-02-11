881a82550be6e9563b8d5f70e468295925468e8d
Joe Torok <fm7222@wayne.edu>
2016-02-11 12:00:17 -0500

    experiments

--- sketch.js
+++ sketch.js
@@ -1,27 +1,22 @@
-var ballpit = [];
+var ballPit = [];
 
-ballpit.push(new Ball(100, 200));
-ballpit.push(new Ball(150, 145));
-ballpit.push(new Ball(200, 100));
-
-ballpit[0].speed.x=2,
-ballpit[0].speed.y=2,
-
-//var myBall = new Ball(100, 100);
-//myBall.speed.x = 2;
-//myBall.speed.y = 2;
-
-//var mySecondBall = new Ball(150, 145);
-
-//var myThirdBall = new Ball (200, 100);
-
-setup = function () {
-  createCanvas(600, 400);
+var setup = function() {
+  createCanvas (800, 800);
 };
+
+var draw = function() {
+  background(50);
+  ballPit.forEach(function updateAndDisplay (ball) {
+    ball.update();
+    ball.display();
+  });
+  if (moiseIsPressed) ballPit.push(new Ball(mouseX, mouse));
+};
+
 
 draw = function () {
   background(100);
-  ballpit.forEach(function updateAndDisplay(ball) {
+  ballPit.forEach(function updateAndDisplay(ball) {
     ball.update();
     ball.display();
 
